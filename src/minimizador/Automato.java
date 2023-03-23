f7c494689bbe6e9e878c91e95cd7665470a56551
pedzero <pedro9669silva@gmail.com>
2023-03-23 18:43:48 -0300

    Método "getAutomato" finalizado.

--- src/minimizador/Automato.java
+++ src/minimizador/Automato.java
@@ -1,51 +1,92 @@
 package minimizador;
 
 import java.io.*;
-import java.util.Scanner;
-
-class Estado {
-
-    public int id;
-}
-
-class Transicao {
-
-    public int id;
-    public int ant;
-    public int next;
-}
 
 public class Automato {
 
-    public int n;
-    public String simbolos[];
-    public Transicao transicoes[];
-    public int inicial;
-    public int finais[];
-    public Estado estados[];
+    public int n, nSimb;
+    public char[][] transicoes;
+    public char[] simbolos;
+    public char inicial;
+    public char[] finais;
 
-    public Automato() {
-        this.n = 0;
+    private static int getPosVetor(char[] vet, char valor) {
+        for (int i = 0; i < vet.length; i++) {
+            if (vet[i] == valor) {
+                return i;
+            }
+        }
+        return -1;
     }
 
-    public void getAutomato(String filePath) throws IOException {
-        BufferedReader buffRead = new BufferedReader(new FileReader(filePath));
-        String linha = "";
-        this.n = 0;
-        while (linha != null) {
-            String[] aux = linha.split(",");
-            if(this.n == 0) {
-                
-            }
-            n = aux.length;
-                for (int i = 0; i < n; i++) {
-                    System.out.println(aux[i]);
-                }
-            
+    public boolean getAutomato(String filePath) throws IOException {
+        try (BufferedReader buffRead = new BufferedReader(new FileReader(filePath))) {
 
-            linha = buffRead.readLine();
+            String linha;
+            short cont = -1;
+
+            while (true) {
+
+                linha = buffRead.readLine();
+                if (linha == null) {
+                    break;
+                }
+                if ((cont == 2) && (!linha.contains(">"))) {
+                    cont--;
+                }
+                cont++;
+
+                String[] aux = linha.split(",");
+                switch (cont) {
+                    case 0 -> {
+                        n = aux.length;
+                        continue;
+                    }
+
+                    case 1 -> {
+                        this.nSimb = aux.length;
+                        simbolos = new char[this.nSimb];
+                        
+                        for (int i = 0; i < this.nSimb; i++) {
+                            simbolos[i] = aux[i].charAt(0);
+                        }
+                        continue;
+                    }
+
+                    case 2 -> {
+                        if (this.transicoes == null) {
+                            this.transicoes = new char[this.n][this.nSimb];
+                        }
+
+                        int origem = Character.getNumericValue(aux[0].charAt(1));
+                        this.transicoes[origem][getPosVetor(simbolos, aux[1].charAt(0))] = aux[2].charAt(1);
+                        continue;
+                    }
+
+                    case 3 -> {
+                        this.inicial = aux[0].charAt(2);
+                        continue;
+                    }
+
+                    case 4 -> {
+                        this.finais = new char[aux.length];
+                        
+                        for (int i = 0; i < aux.length; i++) {
+                            if (i == 0) {
+                                this.finais[i] = aux[i].charAt(2);
+                                continue;
+                            }
+                            this.finais[i] = aux[i].charAt(1);
+                            continue;
+                        }
+                    }
+                }
+            }
+            if (cont != 4) {
+                return false;
+            }
         }
-        buffRead.close();
+        return true;
     }
 }
 
